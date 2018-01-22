d89c73d7e054cdb4a4613a6d77a689cef83d0cf6
Richard Burd <richardburdx@yahoo.com>
2018-01-22 16:00:01 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,53 +1,19 @@
-class FiguresController < ApplicationController
+class LandmarksController < ApplicationController
+  get '/figures' do
+    "here are all the figures"
+  end
 
-    get '/figures' do
-      @figures = Figure.all
-      erb :'figures/index'
-    end
+  get '/figures/new' do
+    "here's a new figure"
+  end
 
-    get '/figures/new' do
-      erb :'figures/new'
-    end
+  get '/figures/:id' do
+    "there is the (params[:id]) you're looking for"
+  end
 
+  get '/figures/:id/edit' do
+    "time to edit this one: (params[:id])"
+  end
 
-    get '/figures/:id' do
-      @figure = Figure.find(params[:id])
-      erb :'figures/show'
-    end
-
-    get '/figures/:id/edit' do
-      @figure = Figure.find(params[:id])
-      erb :'figures/edit'
-    end
-
-    post '/figures' do
-      @figure = Figure.create(params["figure"])
-      if !params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
-
-      if !params[:title][:name].empty?
-        @figure.titles << Title.create(params[:title])
-      end
-
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
-
-    post '/figures/:id' do
-      @figure = Figure.find(params[:id])
-      @figure.update(params[:figure])
-
-      if !params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
-
-      if !params[:title][:name].empty?
-        @figure.titles << Title.create(params[:title])
-      end
-
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
 
 end
